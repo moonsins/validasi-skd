36b80f11935e8b114051d3894de61b797e826016
Moonsins <mfzn31@gmail.com>
2024-02-04 21:31:02 +0700

    last commit

---
+++ database/migrations/2024_02_04_142804_create_dokter_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dokter', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_dokter');
+            $table->string('no_hp');
+            $table->string('kd_spesialis');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dokter');
+    }
+};
